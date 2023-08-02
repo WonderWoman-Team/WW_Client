f23d87e69dc51a8eaba27639bd2b72d19027793f
seminnn <2014178@sookmyung.ac.kr>
2023-08-02 22:32:32 +0900

    feat: UI 수정, Text Color 변경

--- app/src/main/java/com/example/wonderwoman/SchoolActivity.kt
+++ app/src/main/java/com/example/wonderwoman/SchoolActivity.kt
@@ -3,10 +3,7 @@ package com.example.wonderwoman
 import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
-import android.widget.Button
-import android.widget.EditText
-import android.widget.ImageView
-import android.widget.Toast
+import android.widget.*
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
@@ -21,6 +18,17 @@ class SchoolActivity : AppCompatActivity() {
         mBinding = Login21MainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.university,
+            R.layout.spinner_item
+        ).also { adapter ->
+            // Specify the layout to use when the list of choices appears
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            // Apply the adapter to the spinner
+            binding.spinner.adapter = adapter
+        }
+
         //다음페이지
         val intent = Intent(this, EmailActivity::class.java)
         binding.nextBtn1.setOnClickListener{startActivity(intent) }
@@ -34,7 +42,7 @@ class SchoolActivity : AppCompatActivity() {
         // 버튼 이벤트
         btnSend.setOnClickListener() {
             Toast
-                .makeText(this, "toast message", Toast.LENGTH_SHORT)
+                .makeText(this, "인증번호가 메일로 전송되었습니다.", Toast.LENGTH_SHORT)
                 .show()
         }
     }
