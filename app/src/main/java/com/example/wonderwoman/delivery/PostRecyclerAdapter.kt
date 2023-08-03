ecb5df67f85c299388f15d60be0be7e9d83a0e21
seuriseuljjeok <heeseul09@naver.com>
2023-08-03 23:08:30 +0900

    feat: 요청취소/출동취소 클릭 시 리스트 삭제(추후 로직 수정 예정)

--- app/src/main/java/com/example/wonderwoman/delivery/PostRecyclerAdapter.kt
+++ app/src/main/java/com/example/wonderwoman/delivery/PostRecyclerAdapter.kt
@@ -8,15 +8,22 @@ import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.example.wonderwoman.MainActivity
 import com.example.wonderwoman.R
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
-class PostRecyclerAdapter(val PostList: ArrayList<Post>): RecyclerView.Adapter<PostRecyclerAdapter.CustomViewHolder>() {
+class PostRecyclerAdapter(val PostList: ArrayList<Post>) :
+    RecyclerView.Adapter<PostRecyclerAdapter.CustomViewHolder>() {
+    private lateinit var database: FirebaseDatabase
+    private lateinit var databaseReference: DatabaseReference
+    private var index: String = ""
 
     //recyclerView가 Adapter에 연결된 후 최초로 실행되는 부분.
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CustomViewHolder {
         //뷰홀더에 전달
 
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.post_list,parent,false)
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.post_list, parent, false)
         return CustomViewHolder(view)
     }
 
@@ -29,42 +36,50 @@ class PostRecyclerAdapter(val PostList: ArrayList<Post>): RecyclerView.Adapter<P
         holder.time.text = PostList.get(position).time
         holder.nickname.text = PostList.get(position).nickname
         holder.post_state.text = PostList.get(position).post_state
-        if(holder.nickname.text.equals("본인")){
+        if (holder.nickname.text.equals("본인")) {
             holder.go_to_chat_btn.setBackgroundResource(R.drawable.cancel_delivery_btn)
-            holder.go_to_chat_btn.setTextColor(Color.rgb(249,57,95))
-            holder.go_to_chat_btn.setPadding(30,0,0,0)
-            if(holder.post_state.text.equals("요청글")) {
+            holder.go_to_chat_btn.setTextColor(Color.rgb(249, 57, 95))
+            holder.go_to_chat_btn.setPadding(30, 0, 0, 0)
+            if (holder.post_state.text.equals("요청글")) {
                 holder.go_to_chat_btn.text = "요청취소   x"
-            }
-            else if(holder.post_state.text.equals("출동글")){
+            } else if (holder.post_state.text.equals("출동글")) {
                 holder.go_to_chat_btn.text = "출동취소   x"
             }
-        }
-        else{ //본인이 작성한 게시글이 아니라 다른 사람들의 게시글인 경우
+        } else { //본인이 작성한 게시글이 아니라 다른 사람들의 게시글인 경우
             holder.chat_state.text = PostList.get(position).chat_state
 
-            if(holder.post_state.text.equals("요청글")) {
+            if (holder.post_state.text.equals("요청글")) {
                 holder.go_to_chat_btn.text = "요청하기 "
-            }
-            else if(holder.post_state.text.equals("출동글")){
+            } else if (holder.post_state.text.equals("출동글")) {
                 holder.go_to_chat_btn.text = "출동하기 "
             }
 
-            if(holder.chat_state.text.equals("완료")){
+            if (holder.chat_state.text.equals("완료")) {
                 holder.chat_state_img.setBackgroundResource(R.drawable.complete)
                 holder.post_state_img.setBackgroundResource(R.drawable.complete_delivery)
                 holder.chat_state.setTextColor(Color.parseColor("#CCCCCC"))
                 holder.post_state.setTextColor(Color.parseColor("#CCCCCC"))
                 holder.go_to_chat_btn.setBackgroundResource(R.drawable.complete_go_out_btn)
-                holder.go_to_chat_btn.setPadding(20,0,30,0)
-            }
-            else if(holder.chat_state.text.equals("채팅중") || holder.chat_state.text.equals("진행중")){
+                holder.go_to_chat_btn.setPadding(20, 0, 30, 0)
+            } else if (holder.chat_state.text.equals("채팅중") || holder.chat_state.text.equals("진행중")) {
                 holder.chat_state_img.setBackgroundResource(R.drawable.loader)
             }
         }
 
         //리스트 클릭 이벤트
+        //api로 연결하면 로직 변경 예
         holder.go_to_chat_btn.setOnClickListener {
+            if (holder.go_to_chat_btn.text.contains("취소")) {
+                database = FirebaseDatabase.getInstance()
+                databaseReference = database.getReference("Post")
+                if (position in 1..9) index = "0${position}"
+                databaseReference.child("Post_${index}").removeValue().addOnSuccessListener {
+                    notifyItemRemoved(position)
+                    notifyItemRangeRemoved(position, PostList.size - position)
+                }
+            } else {
+
+            }
         }
     }
 
