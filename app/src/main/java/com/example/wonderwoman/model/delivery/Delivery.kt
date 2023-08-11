718e1ad1d1ead94d8b808a8b9ca97b2c19f11ae9
seuriseuljjeok <heeseul09@naver.com>
2023-08-12 06:29:42 +0900

    feat: 딜리버리 게시물 api 연결

---
+++ app/src/main/java/com/example/wonderwoman/model/delivery/Delivery.kt
@@ -0,0 +1,57 @@
+package com.example.wonderwoman.model.delivery
+
+//게시글 목록 관련 데이터 클래스
+
+//response 형식
+data class ResponseDelivery(
+    val content: List<Delivery>,
+    val pageable: Pageable,
+    val first: Boolean,
+    val last: Boolean,
+    val sort: Sort,
+    val number: Int,
+    val size: Int,
+    val numberOfElements: Int,
+    val empty: Boolean
+){
+    data class Delivery(
+        val id: Int,                 //게시물 id
+        val memberId: Int,           //작성자 id
+        val nickname: String,        //작성자 닉네임
+        val createdAt: String,       //게시물 작성 시간
+        val school: String,          //학교명
+        val building: String,        //건물명
+        val postStatus: String,      //진행상태
+        val postTitle: String,       //게시글 제목
+        val postReqType: String,     //게시물 유형
+        val sanitaryNum: Int,        //생리대 개수
+        val sanitarySize: String,    //생리대 사이즈
+        val sanitaryType: String,    //생리대 유형
+        val postComment: String,     //게시물 내용 및 특이사항
+        val written: Boolean         //게시물 조회 시 사용
+    )
+
+    data class Pageable(
+        val sort: Sort,              //
+        val offset: Int,             //
+        val pageSize: Int,           //
+        val pageNumber: Int,         //
+        val paged: Boolean,          //
+        val unpaged: Boolean         //
+    )
+
+    data class Sort(
+        val empty: Boolean,          //
+        val unsorted: Boolean,       //
+        val sorted: Boolean,         //
+    )
+}
+
+
+//
+//data class RequestDelivery(
+//    val reqType: String?,
+//    val building: String?,
+//    val size: List<String>,
+//    val school: String?
+//)
---
+++ app/src/main/java/com/example/wonderwoman/util/Constants.kt
@@ -0,0 +1,10 @@
+package com.example.wonderwoman.util
+
+object Constants {
+    //API
+    const val BASE_URL = "http://ec2-43-202-116-247.ap-northeast-2.compute.amazonaws.com:8080/"
+    //Delivery API
+    const val GET_DELIVERY = "app/delivery/post"
+    //Post API
+    const val POST_POST = "app/delivery"
+}
