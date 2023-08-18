b440acabd49f48d5db9635f98f523d3cfdff8f8d
Somin Yun <somin455@naver.com>
2023-08-19 03:09:50 +0900

    feat: 채팅 목록 api 연결

---
+++ app/src/main/java/com/example/wonderwoman/RetrofitClassChat.kt
@@ -0,0 +1,13 @@
+package com.example.wonderwoman
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitClassChat {
+    private val retrofit2 = Retrofit.Builder()
+        .baseUrl("http://43.202.116.247:8080/")
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
+    val apiRooms: ApiRooms=retrofit2.create(ApiRooms::class.java)
+}
