1a07dedea31fc126d0d73161ab0ea771b3895565
seuriseuljjeok <heeseul09@naver.com>
2023-08-19 21:04:17 +0900

    딜리버리 게시글 목록 ui 수정

--- app/src/main/java/com/example/wonderwoman/model/delivery/Delivery.kt
+++ app/src/main/java/com/example/wonderwoman/model/delivery/Delivery.kt
@@ -18,33 +18,19 @@ data class ResponseDelivery(
     val empty: Boolean
 ){
     data class Delivery(
-//    @SerializedName("content")
         val id: Int,                 //게시물 id
-//    @SerializedName("memberId")
         val memberId: Int,           //작성자 id
-//    @SerializedName("nickname")
         val nickname: String,        //작성자 닉네임
-//    @SerializedName("createdAt")
         val createdAt: String,       //게시물 작성 시간
-//    @SerializedName("school")
         val school: String,          //학교명
-//    @SerializedName("building")
-        val building: List<String>,        //건물명
-//    @SerializedName("postStatus")
+        val building: List<String>,  //건물명
         val postStatus: String,      //진행상태
-//    @SerializedName("postTitle")
         val postTitle: String,       //게시글 제목
-//    @SerializedName("postReqType")
         val postReqType: String,     //게시물 유형
-//    @SerializedName("sanitaryNum")
         val sanitaryNum: Int,        //생리대 개수
-//    @SerializedName("sanitarySize")
         val sanitarySize: String,    //생리대 사이즈
-//    @SerializedName("postReqType")
         val sanitaryType: String,    //생리대 유형
-//    @SerializedName("postComment")
         val postComment: String,     //게시물 내용 및 특이사항
-//    @SerializedName("written")
         val written: Boolean         //게시물 조회 시 사용
     )
 
@@ -58,24 +44,8 @@ data class ResponseDelivery(
     )
 
     data class Sort(
-        val empty: Boolean,          //
-        val unsorted: Boolean,       //
-        val sorted: Boolean,         //
+        val empty: Boolean,
+        val unsorted: Boolean,
+        val sorted: Boolean,
     )
 }
-
-data class RequestAddPost(
-    val building: List<String>,
-    var postTitle: String,
-    var postReqType: String,
-    var sanitaryNum: Int,
-    var sanitarySize: String,
-    var sanitaryType: String,
-    val postComment: String
-)
-
-data class ResponseAddPost(
-    val status: String,
-    val message: String?,
-    val solution: String?
-)
--- app/src/main/java/com/example/wonderwoman/model/post/Post.kt
+++ app/src/main/java/com/example/wonderwoman/model/post/Post.kt
@@ -3,18 +3,19 @@ package com.example.wonderwoman.model.post
 //게시글 관련 데이터 클래스
 
 //request 형식
-data class Post(
+data class ResponseAddPost(
     val status: String,
     val message: String?,
     val solution: String?
 )
 
-data class RequestPost(
-    val building: List<String>,
-    val postTitle: String,
-    val postReqType: String,
-    val sanitaryNum: Int,
-    val sanitarySize: String,
-    val sanitaryType: String,
-    val postComment: String
+data class RequestAddPost(
+    val school: String,
+    var building: List<String>,
+    var postTitle: String,
+    var postReqType: String,
+    var sanitaryNum: Int,
+    var sanitarySize: String,
+    var sanitaryType: String,
+    var postComment: String
 )
