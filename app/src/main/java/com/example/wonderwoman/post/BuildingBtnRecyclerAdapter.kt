505ba4f8f7a81282dce972dcfb4c72699c3d1014
seuriseuljjeok <heeseul09@naver.com>
2023-08-22 01:56:26 +0900

    게시글 작성 내 건물 선택 버튼 학교 별 건물로 설정

--- app/src/main/java/com/example/wonderwoman/post/BuildingBtnRecyclerAdapter.kt
+++ app/src/main/java/com/example/wonderwoman/post/BuildingBtnRecyclerAdapter.kt
@@ -2,25 +2,27 @@ package com.example.wonderwoman.post
 
 import android.content.Context
 import android.content.Intent
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import androidx.recyclerview.widget.RecyclerView
 import com.example.wonderwoman.R
-import com.example.wonderwoman.delivery.PostActivity
-import com.example.wonderwoman.delivery.PostRecyclerAdapter
+import com.example.wonderwoman.model.post.GetSelectedBuilding
 
 class BuildingBtnRecyclerAdapter(
     private val buildingList: List<String>,
-    val context: Context
+    val context: Context,
+    private val getSelectedBuilding: GetSelectedBuilding
 ): RecyclerView.Adapter<BuildingBtnRecyclerAdapter.CustomViewHolder>(){
     override fun onCreateViewHolder(
         parent: ViewGroup,
         viewType: Int
     ): CustomViewHolder {
         //뷰홀더에 전달
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.post_list, parent, false)
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.building, parent, false)
+        Log.d("adapter", buildingList.toString())
         return CustomViewHolder(view)
     }
 
@@ -32,10 +34,14 @@ class BuildingBtnRecyclerAdapter(
         holder.building.text = buildingList[position]
 
         holder.building.setOnClickListener{
-            Intent(context, PostActivity::class.java).apply {
-                putExtra("data", holder.building.text)
-                addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-            }.run { context.startActivity(this) }
+            //postactivity로 선택한 빌딩 보내주
+            getSelectedBuilding.selectedBuilding(holder.building.text.toString())
+            Log.d("adapter", holder.building.text.toString())
+
+//            Intent(context, PostActivity::class.java).apply {
+//                putExtra("data", holder.building.text)
+//                addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+//            }.run { context.startActivity(this) }
         }
     }
 
