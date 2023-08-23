021a4cbedefe59ad9e026bb4012074e71b78ee2d
givesilverstick <108306340+givesilverstick@users.noreply.github.com>
2023-08-23 15:22:26 +0900

    최종

--- app/src/main/java/com/example/wonderwoman/mypage/EditInfoFragment.kt
+++ app/src/main/java/com/example/wonderwoman/mypage/EditInfoFragment.kt
@@ -94,7 +94,8 @@ class EditInfoFragment : Fragment(){
         newNickname.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                requestEditMyInfo.nickname = newNickname.text.toString()
+                if(newNickname.text == null) requestEditMyInfo.nickname = newNickname.hint.toString()
+                else requestEditMyInfo.nickname = newNickname.text.toString()
             }
 
             override fun afterTextChanged(s: Editable?) {}
